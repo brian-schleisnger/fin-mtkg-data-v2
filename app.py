1eef31dee83dd297561083e4a91c83aa41d02344
brian-schleisnger <brian.schlesinger@dish.com>
2026-06-16 13:39:09 -0600

    table question?

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ TABLE_NAME = "ai_dpm_np_sbx.sandbox.2025_fin_mktg_raw"
 
 PGHOST = os.environ.get("PGHOST")
 PGDATABASE = "databricks_postgres" 
-TABLE_NAME = '"sandbox"."2025_fin_mktg_raw"' # Update to whatever you named the synced table
+TABLE_NAME = "2025_fin_mktg_raw" # Update to whatever you named the synced table
 
 # Initialize the SDK Client (auto-authenticates using the App's Service Principal)
 w = WorkspaceClient()
